4693a20e1f12b4fd597a40fb237447ca4ec77446
luchang <chang.lu@sihuatech.com>
2013-06-01 22:15:08 +0800

    dp

--- src/com/kanmenzhu/system/security/action/DepartmentAction.java
+++ src/com/kanmenzhu/system/security/action/DepartmentAction.java
@@ -6,18 +6,18 @@ import org.slf4j.LoggerFactory;
 
 import com.kanmenzhu.system.security.entity.LuDepartment;
 import com.kanmenzhu.system.security.service.DepartmentService;
+import com.kanmenzhu.web.BaseAction;
 import com.opensymphony.xwork2.ActionSupport;
 
-public class DepartmentAction extends ActionSupport {
+public class DepartmentAction extends BaseAction {
 	
-	private Logger logger=LoggerFactory.getLogger(getClass());
-
 	private DepartmentService departmentService;
 	
 	private String name;
 	private String address;
 	private String manager;
 	private String phone;
+	private String dpid;
 	
 	public String regist(){
 		logger.info("####添加单位####");
@@ -32,10 +32,29 @@ public class DepartmentAction extends ActionSupport {
 			department.setAddress(address);
 			department.setPhone(phone);
 			departmentService.save(department);
-			return "success";
+			return SUCC;
 		}else {
 			logger.info("单位名称或负责人为空！");
 			return "regist";
+		}
+	}
+	
+	public String update(){
+		if (StringUtils.isNotBlank(dpid)) {
+			Integer id = Integer.valueOf(dpid);
+			LuDepartment department = departmentService.get(id, LuDepartment.class);
+			if (StringUtils.isNotBlank(name)&&StringUtils.isNotBlank(manager)) {
+				department.setName(name);
+				department.setManager(manager);
+				department.setAddress(address);
+				department.setPhone(phone);
+				departmentService.save(department);
+				
+			}
+			return SUCC;
+		}else {
+			logger.info("单位ID为空！");
+			return "list";
 		}
 	}
 	
@@ -77,6 +96,14 @@ public class DepartmentAction extends ActionSupport {
 
 	public void setDepartmentService(DepartmentService departmentService) {
 		this.departmentService = departmentService;
+	}
+
+	public String getDpid() {
+		return dpid;
+	}
+
+	public void setDpid(String dpid) {
+		this.dpid = dpid;
 	}
 	
 	
