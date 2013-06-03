a831463afbb1261bdbb0c825810c7d600228d530
waydnow <woshishui!3@>
2013-06-03 22:29:12 +0800

    获取当前登录用户

--- src/com/kanmenzhu/web/BaseAction.java
+++ src/com/kanmenzhu/web/BaseAction.java
@@ -1,5 +1,6 @@
 package com.kanmenzhu.web;
 
+import org.apache.struts2.ServletActionContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,16 +14,12 @@ import com.kanmenzhu.system.security.entity.LuUser;
 public class BaseAction {
 	public final String SUCC="success";
 	public final String FAIL="fail";
-	private LuUser currentUser;
 	/**用户登录信息*/
 	public static final String SESSION_USER_INFO="session_user";
 	/***/
 	protected Logger logger=LoggerFactory.getLogger(getClass());
 	/**获取当前登录的用户*/
 	public LuUser getCurrentUser() {
-		return currentUser;
-	}
-	public void setCurrentUser(LuUser currentUser) {
-		this.currentUser = currentUser;
+		return (LuUser)ServletActionContext.getRequest().getSession().getAttribute(SESSION_USER_INFO);
 	}
 }
