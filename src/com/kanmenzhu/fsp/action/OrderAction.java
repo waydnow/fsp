bf18ecdf5373ff0d014dd71c17c9f8b186c3f2f6
waydnow <woshishui!3@>
2013-11-04 23:11:46 +0800

    导出订单

--- src/com/kanmenzhu/fsp/action/OrderAction.java
+++ src/com/kanmenzhu/fsp/action/OrderAction.java
@@ -1,10 +1,28 @@
 package com.kanmenzhu.fsp.action;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+
+import javax.servlet.ServletContext;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.util.CellRangeAddress;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.util.Region;
+import org.apache.struts2.ServletActionContext;
 
 import com.kanmenzhu.fsp.entity.LuGoods;
 import com.kanmenzhu.fsp.entity.LuOrder;
@@ -17,6 +35,7 @@ import com.kanmenzhu.system.security.entity.LuRole;
 import com.kanmenzhu.system.security.service.DepartmentService;
 import com.kanmenzhu.system.security.service.RoleService;
 import com.kanmenzhu.web.BaseAction;
+import com.sun.org.apache.bcel.internal.generic.ANEWARRAY;
 
 public class OrderAction extends BaseAction {
 	
@@ -33,6 +52,10 @@ public class OrderAction extends BaseAction {
 	private List<LuRole> roleList;
 	private Map<Integer, LuDepartment> goodDept;
 	private Map<Integer,LuGoods> goodMap;
+	/** 订单开始时间 */
+	private Date beginTime;
+	/** 订单结束时间 */
+	private Date endTime;
 	
 	private OrderService orderService;
 	private OrderDetailService odetailService;
@@ -230,6 +253,82 @@ public class OrderAction extends BaseAction {
 		return list();
 	}
 	
+	public static String export(){
+		
+		HSSFWorkbook workbook = new HSSFWorkbook();
+		HSSFSheet sheet = workbook.createSheet("采购计划");
+		//第一行
+		HSSFCellStyle styleRow0 = workbook.createCellStyle();
+		Font fontRow0 = workbook.createFont();
+		//设置字体
+		fontRow0.setBoldweight((short)3);
+		fontRow0.setFontName("宋体");
+		fontRow0.setFontHeightInPoints((short)24);
+		styleRow0.setFont(fontRow0);
+		//居中对齐
+		styleRow0.setAlignment(HSSFCellStyle.ALIGN_CENTER);
+		styleRow0.setVerticalAlignment(HSSFCellStyle.VERTICAL_CENTER);
+		HSSFRow row0 = sheet.createRow(0);
+		row0.setHeightInPoints(28);
+		sheet.addMergedRegion(new Region(0,(short)0,0,(short)4));
+		HSSFCell row0Cell0 = row0.createCell(0);
+		row0Cell0.setCellStyle(styleRow0);
+		row0Cell0.setCellValue("20-23号采集计划");
+		//第二行
+		HSSFCellStyle styleRow1 = workbook.createCellStyle();
+		Font fontRow1 = workbook.createFont();
+		//设置字体
+		fontRow1.setFontName("宋体");
+		fontRow1.setFontHeightInPoints((short)16);
+		styleRow1.setFont(fontRow1);
+		HSSFRow row1 = sheet.createRow(1);
+		row1.setHeightInPoints(18);
+		HSSFCell row1Cell0 = row1.createCell(0);
+		row1Cell0.setCellStyle(styleRow1);
+		row1Cell0.setCellValue("学校：");
+		sheet.addMergedRegion(new Region(1, (short)3, 1, (short)4));
+		HSSFCell row1Cell3 = row1.createCell(3);
+		row1Cell3.setCellStyle(styleRow1);
+		row1Cell3.setCellValue("日期：");
+		//第三行
+		HSSFRow row2 = sheet.createRow(2);
+		sheet.addMergedRegion(new Region(2,(short)0,2,(short)4));
+		row2.createCell(0).setCellValue("餐厅经理签字：");
+		//第四行，标题行
+		HSSFRow row3 = sheet.createRow(3);
+		row3.createCell(0).setCellValue("采购物品名称");
+		row3.createCell(1).setCellValue("数量");
+		row3.createCell(2).setCellValue("单位");
+		row3.createCell(3).setCellValue("需送达时间");
+		row3.createCell(4).setCellValue("备注");
+		int num = 4;
+		for (int i = 4; i < num+4; i++) {
+			HSSFRow rowi = sheet.createRow(i);
+			rowi.createCell(0).setCellValue("茄子"+i);
+			rowi.createCell(1).setCellValue("100"+i);
+			rowi.createCell(2).setCellValue("斤"+i);
+			rowi.createCell(3).setCellValue("2012-01-01"+i);
+			rowi.createCell(4).setCellValue("周"+i);
+		}
+		HSSFRow rowlast = sheet.createRow(4+num);
+		rowlast.createCell(0).setCellValue("审批人");
+		rowlast.createCell(3).setCellValue("公章");
+		FileOutputStream file;
+		try {
+			file = new FileOutputStream("workbook.xls");
+			workbook.write(file);
+			file.close();
+		} catch (IOException e) {
+			System.err.println("生产xls文件出错");
+			e.printStackTrace();
+		}
+		return "export";
+	}
+	
+	public static void main(String[] args) {
+		export();
+	}
+	
 	public LuOrder getOrder() {
 		return order;
 	}
@@ -320,6 +419,22 @@ public class OrderAction extends BaseAction {
 
 	public void setRoleList(List<LuRole> roleList) {
 		this.roleList = roleList;
+	}
+
+	public Date getBeginTime() {
+		return beginTime;
+	}
+
+	public void setBeginTime(Date beginTime) {
+		this.beginTime = beginTime;
+	}
+
+	public Date getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(Date endTime) {
+		this.endTime = endTime;
 	}
 	
 	
